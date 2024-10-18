9e1e2394bf7611f50c18e56b68de8cad0eb51132
Marconoyet <ahmed123mah@yahoo.com>
2024-10-18 15:23:18 +0300

    update Structure

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from api import api_bp  # Import the API blueprint
-from db import init_db  # Import the MongoDB initialization function
+from backend.api import api_bp
+from backend.db import init_db  # Import the MongoDB initialization function
 from config import Config  # Import the configuration
 from flask import Flask
 from flask_cors import CORS
